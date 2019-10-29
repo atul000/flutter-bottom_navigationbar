3f18279d53ac9189dfb0f46998ceae4bf1855333
atul <atulbeniwal15@gmail.com>
2019-10-29 21:47:02 +0530

    version 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp();
+    return new MaterialApp(
+      theme:
+          new ThemeData(primarySwatch: Colors.blue, fontFamily: 'RobotoMono'),
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Bottom Nav")),
+      body: new Container(
+        child: Center(
+          child: new RaisedButton(
+            child: new Text(
+              "click here",
+            ),
+            color: Colors.pink[300],
+            onPressed: () => print("i m pressed"),
+          ),
+        ),
+      ),
+    );
   }
 }
