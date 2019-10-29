47d1b72d0337ebdb0a89e8df80916e853be20207
atul <atulbeniwal15@gmail.com>
2019-10-29 22:28:24 +0530

    version 3

--- lib/main.dart
+++ lib/main.dart
@@ -22,16 +22,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Bottom Nav")),
-      body: new Container(
-        child: Center(
-          child: new RaisedButton(
-            child: new Text(
-              "click here",
-            ),
-            color: Colors.pink[300],
-            onPressed: () => print("i m pressed"),
+        appBar: new AppBar(title: new Text("Bottom Nav")), body: new MyBody());
+  }
+}
+
+class MyBody extends StatelessWidget {
+  final AlertDialog dialog = new AlertDialog(
+    content: new Text("Dialog is up..", style: new TextStyle(fontSize: 15.0)),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: Center(
+        child: new RaisedButton(
+          child: new Text(
+            "click here",
           ),
+          color: Colors.pink[300],
+          // onPressed: () => Scaffold.of(context).showSnackBar(new SnackBar(
+          //   content: new Text("hello..whats up"),
+          //   duration: new Duration(seconds: 3),
+          // )),
+          onPressed: () => showDialog(context: context, child: dialog),
         ),
       ),
     );
