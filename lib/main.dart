c4f73ab849d4bc0c3fce1302c5d9b13f8491e384
atul <atulbeniwal15@gmail.com>
2019-10-29 22:47:33 +0530

    version 4

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int current_step = 0;
+
+  List<Step> my_steps = [
+    new Step(
+        title: new Text("Step 1"),
+        content: new Text("this is step 1"),
+        isActive: true),
+    new Step(
+        title: new Text("Step 2"),
+        content: new Text("this is step 2"),
+        isActive: true),
+    new Step(
+        title: new Text("Step 3"),
+        content: new Text("this is step 3"),
+        isActive: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(title: new Text("Bottom Nav")), body: new MyBody());
-  }
-}
-
-class MyBody extends StatelessWidget {
-  final AlertDialog dialog = new AlertDialog(
-    content: new Text("Dialog is up..", style: new TextStyle(fontSize: 15.0)),
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: Center(
-        child: new RaisedButton(
-          child: new Text(
-            "click here",
+        appBar: new AppBar(title: new Text("Bottom Nav")),
+        body: new Container(
+          child: new Stepper(
+            steps: my_steps,
+            currentStep: this.current_step,
+            type: StepperType.vertical,
           ),
-          color: Colors.pink[300],
-          // onPressed: () => Scaffold.of(context).showSnackBar(new SnackBar(
-          //   content: new Text("hello..whats up"),
-          //   duration: new Duration(seconds: 3),
-          // )),
-          onPressed: () => showDialog(context: context, child: dialog),
-        ),
-      ),
-    );
+        ));
   }
 }
+
+// class MyBody extends StatelessWidget {
+//   final AlertDialog dialog = new AlertDialog(
+//     content: new Text("Dialog is up..", style: new TextStyle(fontSize: 15.0)),
+//     title: new Text("hello"),
+//   );
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Container(
+//       child: Center(
+//         child: new RaisedButton(
+//           child: new Text(
+//             "click here",
+//           ),
+//           color: Colors.pink[300],
+// onPressed: () => Scaffold.of(context).showSnackBar(new SnackBar(
+//   content: new Text("hello..whats up"),
+//   duration: new Duration(seconds: 3),
+// )),
+//           onPressed: () => showDialog(context: context, child: dialog),
+//         ),
+//       ),
+//     );
+//   }
+// }
